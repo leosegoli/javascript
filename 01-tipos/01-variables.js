7cf8b5558dcefa0eb866b92b3544390c09ce2f83
leosegoli <yngwieleo@gmail.com>
2024-01-12 09:45:28 -0300

    primer commit

---
+++ 01-tipos/01-variables.js
@@ -0,0 +1,13 @@
+let nombre = "Hola Mundo";
+
+let NombreCompleto;
+let nombreCompleto;
+let nombre_completo;
+
+console.log(nombre);
+
+let apellido
+let animal;
+apellido = 'Segoli';
+animal = 'Perro';
+
---
+++ 01-tipos/03-constantes.js
@@ -0,0 +1,5 @@
+const nombre = 'Hola Mundo'; //si inicializamos una variable con const no se puede cambiar su valor
+
+// nombre = 'chanchito'; no se puede cambiar el valor de una constante
+
+console.log(nombre);
