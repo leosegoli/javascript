02eed88b9e149d31ff171cd673bf9a2d91f82457
leosegoli <yngwieleo@gmail.com>
2024-01-16 08:41:15 -0300

    segundo

---
+++ 03-control-flujo/01-if-else.js
@@ -0,0 +1,9 @@
+let edad = 25;
+
+if (edad>17){
+    console.log('usuario mayor de edad');
+} else if (edad > 13) {
+    console.log('el usuario debe ir acompañado de sus padres')    
+} else {
+    console.log('usuario menor de edad')
+}   
